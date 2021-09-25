6fff6ef2bb1d46cd7845ef432deeee941d54cfaa
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-25 14:16:33 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require(),
     url="https://github.com/bbugyi200/logutils",
-    use_scm_version={"fallback_version": "0.1.0"},
+    use_scm_version={"fallback_version": "0.1.1"},
     zip_safe=False,
 )
