3c9e71f06a2b85fc7470264864b2978051bd1b36
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-30 13:28:04 -0400

    Update cc via 'cruft update'

--- src/logutils/__init__.py
+++ src/logutils/__init__.py
@@ -4,10 +4,7 @@ Better logging made easy with support for structlog and the standard logging
 module.
 """
 
-__author__ = "Bryan M Bugyi"
-__email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.1.4"
-
+import logging as _logging
 
 from .logutils import (
     BetterBoundLogger,
@@ -18,3 +15,10 @@ from .logutils import (
     get_default_logfile,
     init_logging,
 )
+
+
+__author__ = "Bryan M Bugyi"
+__email__ = "bryanbugyi34@gmail.com"
+__version__ = "0.1.4"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
